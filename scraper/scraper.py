1ec1d38d09cd7733e12ee4d7911c2067a304b31d
Ihor1654 <122479591+Ihor1654@users.noreply.github.com>
2024-10-10 16:03:33 +0200

    Add comments and some ruslting files

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -10,13 +10,27 @@ from src.link_creator import RunThread
 
 class Scraper():
     """
-    
+    Class that take links from json file and scrap data from them to another json
+    :properties:
+    headers
+    json_path
+    timeout
+    semaphore
+    link_dict
+    houses_raw_data_dict
+    old_links_counter
+    problem_with_json_list
+    houses_cleaned_data_list
+    :methods:
+    upload_data_from_json()
+    run_as()
+    get_house_raw_data()
+    get_house_raw_data_for_pc()
+    get_full_raw_data_set()
+    clean_up_house_data()
+    clean_up_all_data()
+    raw_data_to_json()
     """
-
-
-
-
-
 
     def __init__(self) -> None:
         self.headers = {"User-Agent":'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:130.0) Gecko/20100101 Firefox/130.0'}
@@ -29,7 +43,12 @@ class Scraper():
         self.problem_with_json_list = []
         self.houses_cleaned_data_list = []
 
-    def upload_data_from_json(self, filepath : str = ''):
+    def upload_data_from_json(self, filepath : str = '') -> dict:
+        """
+            Method that uploads a dictionary from the house_for_postcode.json file.
+            :param filepath: str, the file path to the JSON file with house links, default is house_for_postcode.json.
+            :return: link_dict, a dictionary where keys are postcodes and values are lists of links for specific houses.
+        """
         if filepath == '':
             filepath = self.json_path
         with open(filepath,'r') as f:
@@ -54,7 +73,12 @@ class Scraper():
             print('Starting new event loop')
             return asyncio.run(coro)
 
-    async def get_house_raw_data(self,link,session):
+    async def get_house_raw_data(self, link : str, session : httpx.AsyncClient) -> None:
+        """
+            Method-coroutine that sends a request to the website and adds scraped data from the response to the dictionary.
+            :param link: str, contains the link to the specific house.
+            :param session: AsyncClient, an instance of the class used to make the request.
+        """
         while True:
             try:
                 resp = await session.get(link, headers=self.headers, timeout = self.timeout)
@@ -85,19 +109,32 @@ class Scraper():
             except httpx.TimeoutException:
                 print(f'Request time out for {link}')
 
-
-    async def get_house_raw_data_for_pc(self,key):
+    async def get_house_raw_data_for_pc(self, key : str) -> None:
+        """
+            Method coroutine that creates tasks from get_house_raw_data coroutines for a specific postcode and runs them concurrently.
+            :param key: str, the postcode key from the link_dict.
+            :return: None
+        """  
         async with self.semaphore:
             async with httpx.AsyncClient() as session:
                 tasks = [asyncio.create_task(self.get_house_raw_data(link,session)) for link in self.link_dict[key]]
                 resp = await asyncio.gather(*tasks)
 
-
-    async def get_full_raw_data_set(self):
+    async def get_full_raw_data_set(self) -> None:
+        """
+            Method coroutine that creates tasks from get_house_raw_data_for_ps coroutines for all postcodes and runs them concurrently.
+            :return: None
+        """
         tasks = [asyncio.create_task(self.get_house_raw_data_for_pc(key)) for key in self.link_dict.keys()]
         resp = await asyncio.gather(*tasks)
     
-    def clean_up_house_data(self,house_raw_dict : dict, link : str):
+    def clean_up_house_data(self,house_raw_dict : dict, link : str) -> dict:
+        """
+            Method that takes data for a specific link from the dictionary with raw data, cleans it up, and returns a resulting dictionary with 18 fields.
+            :param link: str, link to a specific house.
+            :param house_raw_dict: dict, dictionary with raw data for specific houses.
+            :return: dict my_dict with cleaned data.
+        """
         my_dict = {'link':f"{link}",}
         if not house_raw_dict["flags"]["isLifeAnnuitySale"]:
             my_dict['property_id'] = house_raw_dict["id"]
@@ -159,7 +196,11 @@ class Scraper():
         else:
             return None
 
-    def clean_up_all_data(self):
+    def clean_up_all_data(self) -> list:
+        """
+        Method that reads all dictionaries with cleaned data from data/raw_data_houses.json and adds them to a list for creating a CSV file.
+        :return: list houses_cleaned_data_list, a list of dictionaries with cleaned data.
+        """
         with open('data/raw_data_houses.json','r') as file:
             goal_dict = json.load(file)
         for key,dicti in goal_dict.items():
@@ -168,10 +209,11 @@ class Scraper():
                 self.houses_cleaned_data_list.append(cleand_dict)
         return self.houses_cleaned_data_list
 
-                
-             
-    
-    def raw_data_to_json(self,filename : str = 'data/raw_data_houses.json'):
+    def raw_data_to_json(self,filename : str = 'data/raw_data_houses.json') -> None:
+        """
+        Method that writes raw data from houses_raw_data_dict to a JSON file.
+        :param filename: str, filepath to the future file.
+        """
         with open(filename,'w') as file:
             json.dump(self.houses_raw_data_dict,file,ensure_ascii=False)
 
